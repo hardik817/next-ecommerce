b07a398b37fb32b07c14c6a5178f67ebffdd2665
HARDIK SACHDEVA <sachdevahardik22@gmail.com>
2025-01-01 15:01:47 +0530

    mobile nav complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Menu from "./menu";
 
 const Navbar = () => {
     return (
-        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
             {/*mobile*/}
             <div className="h-full flex items-center justify-between">
                 <Link href="/">
@@ -11,6 +11,7 @@ const Navbar = () => {
                 </Link>
                 <Menu />
             </div>
+            {/*Bigger Screens*/}
         </div >
     )
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,8 +9,8 @@ const Menu = () => {
         <div className=''>
             <Image src="/menu.png" alt="" width={28} height={28} className='cursor-pointer' onClick={() => SetOpen((prev) => !prev)}></Image>
             {
-                open && (<div className="">
-                    <Link href="/">HomePage</Link>
+                open && (<div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10">
+                    <Link href="/">Home</Link>
                     <Link href="/">Shop</Link>
                     <Link href="/">Deals</Link>
                     <Link href="/">About</Link>
